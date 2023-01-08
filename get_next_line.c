1131a6ab0096aa75a4759e26b5d12d8edd2b82b8
Jose Nuncio Cecilio Calejo Pires <jnuncio-@c1r7s1.42lisboa.com>
2023-01-08 15:58:34 +0000

    created ft_get_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,34 @@
 /*   By: jnuncio- <jnuncio-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 16:13:42 by jnuncio-          #+#    #+#             */
-/*   Updated: 2022/12/26 23:39:05 by jnuncio-         ###   ########.fr       */
+/*   Updated: 2023/01/08 15:56:41 by jnuncio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*ft_get_line(char *stash)
+{
+	int		i;
+	char	*line;
+
+	i = 0;
+	if (!stash)
+		return (NULL);
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	line = (char *)malloc(sizeof(char *) * (i + 1));
+	if (!line)
+		return (NULL);
+	i = -1;
+	while (stash[++i] && stash[i] != '\n')
+		line[i] = stash[i];
+	if (stash[i] == '\n')
+		line[i] = stash[i];
+	line[++i] = '\0';
+	stash = ft_substr(stash, i, ft_strlen(stash) - i);
+	return (line);
+}
 
 char	*get_next_line(int fd)
 {
@@ -18,46 +41,35 @@ char	*get_next_line(int fd)
 	static char	*stash;
 	char		*line;
 	int			r;
-	size_t		i;
 
-	r = -1;
-	i = -1;
-	buf = malloc(BUFFER_SIZE + 1);
-	while (r != 0)
+	buf = (char *)malloc(BUFFER_SIZE + 1);
+	if (!buf)
+		return (NULL);
+	r = read(fd, buf, BUFFER_SIZE);
+	if (r == -1)
+		return (NULL);
+	else if (r != 0)
 	{
-		while (stash[++i])
-		{
-			if (stash[i] == '\n')
-			{
-				line = ft_substr(stash, 0, i);
-				free(stash);
-				stash = ft_substr(stash, i + 1, ft_strlen(stash) - i);
-				if (!line || !stash)
-					return (NULL);
-			}
-		}
-		r = read(fd, buf, BUFFER_SIZE);
-		if (r == -1 || fd < 0)
-			return (NULL);
 		stash = ft_strjoin(stash, buf);
+		get_next_line(fd);
 	}
-	free(buf);
+	line = ft_get_line(stash);
 	return (line);
 }
 
-int	main(void)
-{
-	int		fd;
-	char	*line;
+// int	main(void)
+// {
+// 	int		fd;
+// 	char	*line;
 
-	fd = open("test.txt", O_RDONLY);
-	line = get_next_line(fd);
-	while (line)
-	{
-		printf("%s\n", line);
-		free(line);
-	}
-	// printf("\n");
-	// while (get_next_line(0))
-	// 	printf("%s\n", get_next_line(0));
-}
+// 	fd = open("test.txt", O_RDONLY);
+// 	printf("fd = %d/n", fd);
+// 	line = get_next_line(fd);
+// 	printf("%s\n", line);
+// 	while (line)
+// 		printf("%s\n", line);
+// 	// printf("\n");
+// 	// while (get_next_line(0))
+// 	// 	printf("%s\n", get_next_line(0));
+// 	return (0);
+// }
